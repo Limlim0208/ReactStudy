9721b29e69e153d3cab9eba6482442972eb01869
임유미 <dladbal24@sookmyung.ac.kr>
2026-04-16 10:47:34 +0900

    08-03: 실습 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import "./App.css";
-import Global from "./components/Global";
+import styles from "./App.module.css";
 
 export default function App() {
     return (
         <>
-            <h1 className="title">App Component</h1>
-            <Global />
+            <h1 className={styles.title}>App Component</h1>
         </>
     );
 }

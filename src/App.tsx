0ef8f06c0c4f544ea33b957f563e8366e67a94c2
임유미 <dladbal24@sookmyung.ac.kr>
2026-04-17 10:26:39 +0900

    08-11: 실습 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 import { keyframes } from "styled-components";
+import { css } from "styled-components";
+
+const boxShadowMixin = css`
+    box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.5);
+`;
 
 const fadeIn = keyframes`
 from{
@@ -17,7 +22,7 @@ const Title = styled.h1<{ $color: string; $decoration: string }>`
 `;
 
 const BigTitle = styled(Title)`
-    font-size: 50px;
+    font-size: ${(props) => props.theme.fontSize.large};
 `;
 
 const Wrapper = styled.section`
@@ -25,14 +30,15 @@ const Wrapper = styled.section`
     border: 1px solid red;
 `;
 
-const BlueBorderWrapper = styled(Wrapper)`
-    border-color: blue;
+const BlueBorderWrapper = styled(Wrapper)<{ $shadow: boolean }>`
+    border-color: ${(props) => props.theme.colors.primary};
+    ${(props) => props.$shadow && boxShadowMixin}
 `;
 
 export default function App() {
     return (
         <>
-            <BlueBorderWrapper>
+            <BlueBorderWrapper $shadow>
                 <Title $color="#0000ff" $decoration="line-through" as="p">
                     Hello, ReactJS!
                 </Title>

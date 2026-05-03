d9cac8ae295e8d0890374ffdc00cad5f9d5cbe0f
임유미 <dladbal24@sookmyung.ac.kr>
2026-05-03 17:25:47 +0900

    10-01: 실습 코드

--- src/components/controlled/Input.tsx
+++ src/components/controlled/Input.tsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+
 export default function Input() {
-  return (
-    <>
-      <form>
-        <h1>Input: </h1>
-        <input type="text" />
-      </form>
-    </>
-  );
+    const [formState, setFormState] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleFormStateChange = (
+        e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+    ) => {
+        setFormState((formState) => ({
+            ...formState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    return (
+        <>
+            <form>
+                <h1>
+                    Input:{formState.email}/{formState.password}{" "}
+                </h1>
+                <input
+                    type="text"
+                    name="email"
+                    value={formState.email}
+                    onChange={handleFormStateChange}
+                />
+                <input
+                    type="text"
+                    name="password"
+                    value={formState.password}
+                    onChange={handleFormStateChange}
+                />
+            </form>
+        </>
+    );
 }

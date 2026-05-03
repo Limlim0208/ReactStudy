cf1cc9991b7d529e6db05b00bc111916ae6caeea
임유미 <dladbal24@sookmyung.ac.kr>
2026-05-03 16:44:09 +0900

    08-14: 실습 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,41 @@
-import styled from "styled-components";
-import { keyframes } from "styled-components";
-import { css } from "styled-components";
-
-const boxShadowMixin = css`
-    box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.5);
-`;
-
-const fadeIn = keyframes`
-from{
-  opacity: 0;
-}
-to{
-  opacity: 1;
-}
-`;
-
-const Title = styled.h1<{ $color: string; $decoration: string }>`
-    color: ${(props) => props.$color};
-    text-decoration: ${(props) => props.$decoration};
-    animation: ${fadeIn} 2s ease-in;
-    &:hover {
-        color: pink;
-    }
-`;
-
-const BigTitle = styled(Title)`
-    font-size: ${(props) => props.theme.fontSize.large};
-`;
-
-const Wrapper = styled.section`
-    padding: 2rem;
-    border: 1px solid red;
-`;
-
-const BlueBorderWrapper = styled(Wrapper)<{ $shadow: boolean }>`
-    border-color: ${(props) => props.theme.colors.primary};
-    ${(props) => props.$shadow && boxShadowMixin}
-`;
+import { useState } from "react";
 
 export default function App() {
+    // 초기 버튼 데이터: 각 버튼은 id, label, icon, isDisabled 속성을 가집니다.
+    const initialButtons = [
+        { id: 1, label: "Button 1", icon: "🔥", isDisabled: false },
+        { id: 2, label: "Button 2", icon: "💧", isDisabled: false },
+        { id: 3, label: "Button 3", icon: "🌱", isDisabled: true },
+        { id: 4, label: "Button 4", icon: "⚡", isDisabled: false },
+    ];
+
+    const [buttons, setButtons] = useState(initialButtons);
+
+    const toggleHandle = (currentButtonId: number) => {
+        setButtonState(
+            buttons.filter((button) => button.id !== currentButtonId),
+        );
+    };
+
     return (
         <>
-            <BlueBorderWrapper $shadow>
-                <Title
-                    $color="#0000ff"
-                    $decoration="line-through"
-                    as="p"
-                    title="...hello..."
-                    onClick={() => alert("hello!")}
-                >
-                    Hello, ReactJS!
-                </Title>
-                <BigTitle $color="#0000ff" $decoration="line-through">
-                    Hello, Big Title!
-                </BigTitle>
-            </BlueBorderWrapper>
+            <ButtonGroup buttons={buttons}></ButtonGroup>
+        </>
+    );
+}
+
+export function ButtonGroup({
+    buttons,
+}: { id: number; label: string; icon: string; isDisabled: boolean }[]) {
+    return (
+        <>
+            <h2>Active Count: {}</h2>
+            {buttons.map((button) => (
+                <button key={button.id} disabled={button.isDisabled}>
+                    {button.icon}
+                    {button.label}
+                </button>
+            ))}
         </>
     );
 }
